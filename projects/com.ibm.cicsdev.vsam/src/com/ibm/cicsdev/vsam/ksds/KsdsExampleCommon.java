441b92ed27214082a1c5fcd7952b665424b6e985
Ian Burnett <ian.burnett@uk.ibm.com>
2017-01-11 11:07:33 +0000

    Add copyright header. (#9)

--- projects/com.ibm.cicsdev.vsam/src/com/ibm/cicsdev/vsam/ksds/KsdsExampleCommon.java
+++ projects/com.ibm.cicsdev.vsam/src/com/ibm/cicsdev/vsam/ksds/KsdsExampleCommon.java
@@ -1,3 +1,13 @@
+/* Licensed Materials - Property of IBM                                   */
+/*                                                                        */
+/* SAMPLE                                                                 */
+/*                                                                        */
+/* (c) Copyright IBM Corp. 2017 All Rights Reserved                       */
+/*                                                                        */
+/* US Government Users Restricted Rights - Use, duplication or disclosure */
+/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
+/*                                                                        */
+
 package com.ibm.cicsdev.vsam.ksds;
 
 import java.util.ArrayList;
