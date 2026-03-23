f42677b7f0638d6ceae927ab955731904d691cbb
Phil Wakelin <phil_wakelin@uk.ibm.com>
2026-03-23 15:12:37 +0000

    Add VSAM support

---
+++ cics-java-jcics-vsam-app/src/main/java/com/ibm/cicsdev/vsam/esds/EsdsExample4.java
@@ -0,0 +1,37 @@
+/* Licensed Materials - Property of IBM                                   */
+/*                                                                        */
+/* SAMPLE                                                                 */
+/*                                                                        */
+/* (c) Copyright IBM Corp. 2017 All Rights Reserved                       */
+/*                                                                        */
+/* US Government Users Restricted Rights - Use, duplication or disclosure */
+/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
+/*                                                                        */
+
+package com.ibm.cicsdev.vsam.esds;
+
+import com.ibm.cics.server.Task;
+
+
+/**
+ * This is a dummy test class to remain consistent with the KSDS and RRDS examples.
+ * 
+ * Records cannot be deleted from an ESDS dataset, either in JCICS or in the
+ * EXEC CICS API.
+ */
+public class EsdsExample4 
+{
+    public static void main(String[] args)
+    {
+        // Get details about our current CICS task
+        Task task = Task.getTask();
+        task.out.println(" - Starting EsdsExample4");
+        task.out.println("VSAM ESDS record delete example");
+        
+        // VSAM ESDS does not allow record deletion
+        task.out.println("Record deletion not permitted for VSAM ESDS files");
+        
+        // Completion message
+        task.out.println("Completed EsdsExample4");
+    }
+}
---
+++ etc/VSAM/README.md
@@ -0,0 +1,5 @@
+Supporting materials used in conjunction with the JCICS VSAM samples.
+
+* DEFVSAM.jcl - Sample job to define the KSDS, ESDS, and RRDS VSAM files required.
+* DFHCSD.txt - Output from a DFHCSDUP EXTRACT operation to define the programs and transactions required.
+
